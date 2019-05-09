bb77b0b5dc6e0cf5efbe49b53a588dbfaafef0e8
KenshiroTammachi <flofloyui.ecosysken@icloud.com>
2019-05-10 00:22:24 +0900

    inital commit

--- main.py
+++ main.py
@@ -44,10 +44,14 @@ def callback():
 # MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text='「' + event.message.text + '」って何？')
-     )
+        if event.message.text=="おっぱい":
+            event.reply_token,
+            TextSendMessage(text='もみもみ')
+        else
+	       line_bot_api.reply_message(
+           event.reply_token,
+           TextSendMessage(text='「' + event.message.text + '」って何？')
+)
 if __name__ == "__main__":
     port = int(os.getenv("PORT"))
     app.run(host="0.0.0.0", port=port)
