c830c8d12936013a350c3b910361084b2696c881
KenshiroTammachi <flofloyui.ecosysken@icloud.com>
2019-05-23 11:11:25 +0900

    untiunti

--- main.py
+++ main.py
@@ -45,50 +45,18 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if '位置情報' in event.message.text:
-        {
-    "type": "template",
-    "altText": "this is a carousel template",
-    "template": {
-        "type": "carousel",
-        "actions": [],
-        "columns": [
-            {
-                "thumbnailImageUrl": "SPECIFY_YOUR_IMAGE_URL",
-                "title": "タイトルです",
-                "text": "テキストです",
-                "actions": [
-                    {
-                        "type": "message",
-                        "label": "アクション 1",
-                        "text": "アクション 1"
-                    },
-                    {
-                        "type": "message",
-                        "label": "アクション 2",
-                        "text": "アクション 2"
-                    }
-                ]
-            },
-            {
-                "thumbnailImageUrl": "SPECIFY_YOUR_IMAGE_URL",
-                "title": "タイトルです",
-                "text": "テキストです",
-                "actions": [
-                    {
-                        "type": "message",
-                        "label": "アクション 1",
-                        "text": "アクション 1"
-                    },
-                    {
-                        "type": "message",
-                        "label": "アクション 2",
-                        "text": "アクション 2"
-                    }
-                ]
-            }
-        ]
-    }
-}
+        $template = array('type'    => 'buttons',
+                  'thumbnailImageUrl' => 'https://d1f5hsy4d47upe.cloudfront.net/79/79e452da8d3a9ccaf899814db5de9b76_t.jpeg',
+                  'title'   => 'タイトル最大40文字' ,
+                  'text'    => 'テキストメッセージ。タイトルがないときは最大160文字、タイトルがあるときは最大60文字',
+                  'actions' => array(
+                                 array('type'=>'message', 'label'=>'ラベルです', 'text'=>'アクションを実行した時に送信されるメッセージ' )
+                                )
+                );
+        $message = array('type'     => 'template',
+                 'altText'  => '代替テキスト',
+                 'template' => $template
+                );
     else:
         content = '「' + event.message.text +'」はおっぱいじゃないです'
     line_bot_api.reply_message(
