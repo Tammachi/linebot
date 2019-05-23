7f38fee1f9bb464457ce92bc3b9d2275a852bea1
KenshiroTammachi <flofloyui.ecosysken@icloud.com>
2019-05-23 14:45:03 +0900

    unti

--- main.py
+++ main.py
@@ -44,37 +44,74 @@ def callback():
 # MessageEvent
 def make_carousel_template():
     message_template = TemplateSendMessage(
-        "type": "template",
-    "altText": "this is a image carousel template",
-    "template": {
-        "type": "image_carousel",
-        "columns": [
+        {
             {
-                "imageUrl": "https://example.com/test-cline/b01_img01.jpg",
-                "action": {
-                    "type": "uri",
-                    "label": "View detail",
-                    "uri": "https://classmethod.jp/"
-                }
+  "type": "template",
+  "altText": "this is a carousel template",
+  "template": {
+      "type": "carousel",
+      "columns": [
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
+            "imageBackgroundColor": "#FFFFFF",
+            "title": "this is menu",
+            "text": "description",
+            "defaultAction": {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/123"
             },
-            {
-                "imageUrl": "https://example.com/test-cline/b01_img02.jpg",
-                "action": {
+            "actions": [
+                {
                     "type": "postback",
                     "label": "Buy",
                     "data": "action=buy&itemid=111"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=111"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/111"
                 }
+            ]
+          },
+          {
+            "thumbnailImageUrl": "https://example.com/bot/images/item2.jpg",
+            "imageBackgroundColor": "#000000",
+            "title": "this is menu",
+            "text": "description",
+            "defaultAction": {
+                "type": "uri",
+                "label": "View detail",
+                "uri": "http://example.com/page/222"
             },
-            {
-                "imageUrl": "https://example.com/test-cline/b01_img04.jpg",
-                "action": {
-                    "type": "message",
-                    "label": "Yes",
-                    "text": "yes"
+            "actions": [
+                {
+                    "type": "postback",
+                    "label": "Buy",
+                    "data": "action=buy&itemid=222"
+                },
+                {
+                    "type": "postback",
+                    "label": "Add to cart",
+                    "data": "action=add&itemid=222"
+                },
+                {
+                    "type": "uri",
+                    "label": "View detail",
+                    "uri": "http://example.com/page/222"
                 }
-            }
-        ]
-    }
+            ]
+          }
+      ],
+      "imageAspectRatio": "rectangle",
+      "imageSize": "cover"
+  }
+}
     )
     return message_template
 
