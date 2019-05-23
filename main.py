2068ec4b285580394e63aaf9024d3f024b1b8271
KenshiroTammachi <flofloyui.ecosysken@icloud.com>
2019-05-23 11:18:17 +0900

    untiunti

--- main.py
+++ main.py
@@ -45,18 +45,27 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if '位置情報' in event.message.text:
-        $template = array('type'    => 'buttons',
-                  'thumbnailImageUrl' => 'https://d1f5hsy4d47upe.cloudfront.net/79/79e452da8d3a9ccaf899814db5de9b76_t.jpeg',
-                  'title'   => 'タイトル最大40文字' ,
-                  'text'    => 'テキストメッセージ。タイトルがないときは最大160文字、タイトルがあるときは最大60文字',
-                  'actions' => array(
-                                 array('type'=>'message', 'label'=>'ラベルです', 'text'=>'アクションを実行した時に送信されるメッセージ' )
-                                )
-                );
+        $columns = array(
+                        array('thumbnailImageUrl' => '画像のURL',
+                            'title'   => 'タイトル最大４０文字',
+                            'text'    => 'タイトルか画像がある場合は最大60文字、どちらもない場合は最大120文字',
+                            'actions' => array(array('type' => 'message', 'label' => 'ラベルです', 'text' => 'メッセージ'))
+                 ),
+                 array('thumbnailImageUrl' => '画像のURL',
+                       'title'   => 'タイトル最大４０文字',
+                       'text'    => 'タイトルか画像がある場合は最大60文字、どちらもない場合は最大120文字',
+                       'actions' => array(array('type' => 'message', 'label' => 'ラベルです', 'text' => 'メッセージ'))
+                 )
+            );
+
+        $template = array('type'    => 'carousel',
+                    'columns' => $columns,
+                    );
+
         $message = array('type'     => 'template',
-                 'altText'  => '代替テキスト',
-                 'template' => $template
-                );
+                    'altText'  => '代替テキスト',
+                    'template' => $template
+                    );
     else:
         content = '「' + event.message.text +'」はおっぱいじゃないです'
     line_bot_api.reply_message(
