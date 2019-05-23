72ff973fd9088bf26f34302b1a6fa1e4d248f1c2
KenshiroTammachi <flofloyui.ecosysken@icloud.com>
2019-05-23 19:07:28 +0900

    unti

--- main.py
+++ main.py
@@ -98,7 +98,7 @@ def handle_message(event):
         messages = make_carousel_template()
     line_bot_api.reply_message(
         event.reply_token,
-        messages
+        messages = messages
         )
 
 if __name__ == "__main__":
