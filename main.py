ec721ae43cadbd1b3612ebb163d8e920659337a0
KenshiroTammachi <flofloyui.ecosysken@icloud.com>
2019-06-23 10:55:46 +0900

    unti

--- main.py
+++ main.py
@@ -217,7 +217,8 @@ def handle_message(event):
         route_search_latitude=999
         route_search_longitude=999
     else:
-        content = 'うんちうんち'
+        date = read_data()
+        content = data[1][12]
 
     line_bot_api.reply_message(
         event.reply_token,
