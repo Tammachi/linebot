28dff9b258b6ded9ccf910624d672c8a298f3fb0
KenshiroTammachi <flofloyui.ecosysken@icloud.com>
2019-06-23 10:19:34 +0900

    unti

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def make_carousel_template():
                 CarouselColumn(
                     thumbnail_image_url='https://upload.wikimedia.org/wikipedia/commons/3/35/Kiyomizu_Temple_-_01.jpg',
                     title=data[1][8],
-                    text=data[1][2],
+                    text=data[1][12],
                     actions=[
                         PostbackAction(
                             label='ここに行く！',
