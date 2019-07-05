7f0b715492c833df7f552ecf1c6b00ccee2dd0a4
KenshiroTammachi <s16536@salesio-sp.ac.jp>
2019-07-05 19:54:15 +0900

    unti

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ def make_carousel_template(address):
                             text=data[num[2]][3]
                         ),
                         URIAction(
-                            label='uri1',
+                            label='ここに行く！(直接)',
                             uri=URL[1]
                         )
                     ]
