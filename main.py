c0beb4128916a1c967a10449c5fc9e8da081282e
KenshiroTammachi <s16536@salesio-sp.ac.jp>
2019-07-29 22:08:17 +0900

    aaa

--- main.py
+++ main.py
@@ -249,7 +249,7 @@ def handle_message(event):
         description = description.rstrip('について教えて！')
         content = description
     else:
-        gmaps = googlemaps.Client(key=GOOGLE_API_KEY)
+        gmaps = googlemaps.Client(key=ENV[GOOGLE_API_KEY])
         address = event.message.text
         result = gmaps.geocode(address)
         lat = result[0]["geometry"]["location"]["lat"]
