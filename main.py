99196890718ded4dc02a4c45979d0616f959394d
KenshiroTammachi <s16536@salesio-sp.ac.jp>
2019-08-01 11:45:32 +0900

    aaa

--- main.py
+++ main.py
@@ -86,13 +86,13 @@ def make_kyori(lat,lng,lat2,lng2):
     distance = result['rows'][0]['elements'][0]['distance']['value']
     #distance += result['rows'][0]['elements'][0]['duration']['value']
     duration = result['rows'][0]['elements'][0]['duration']['value']
-    duration = datetime.timedelta(seconds=duration)
+    time = datetime.timedelta(seconds=duration)
     # if distance > 1000:
     #     distance = distance/1000
     #     distance = str(distance) + "km "
     # else:
     #     distance = str(distance) + "m "
-    explanation = str(distance) + str(duration)
+    explanation = str(distance) + str(time)
     return explanation
 
 #言葉から、areaを探す。（未完）
