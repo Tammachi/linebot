8e25a484d71cbc077fc1f5e353bb9e88ee6452b8
KenshiroTammachi <s16536@salesio-sp.ac.jp>
2019-08-01 11:12:07 +0900

    a

--- main.py
+++ main.py
@@ -88,7 +88,8 @@ def make_kyori(lat,lng,lat2,lng2):
     duration = result['rows'][0]['elements'][0]['duration']['value']
     duration = datetime.timedelta(seconds=duration)
     if distance > 1000:
-        distance = str(distance/1000) + "km "
+        distance = distance/1000
+        distance = str(distance) + "km "
     else:
         distance = str(distance) + "m"
     explanation =  distance + str(duration)
